da1202533912ceb820ebce89a93637ed87a46bd9
takeshikimura5255 <takeshikimura525+5@gmail.com>
2025-02-22 15:45:49 +0900

    fix:github changed

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -35,11 +35,11 @@ export const socialData = [
   //   link: "https://pinterest.com",
   //   Icon: RiPinterestLine,
   // },
-  {                                     ///////////Github
-    name: "Github",
-    link: "https://github.com/NakamuraKuhiniko",
-    Icon: RiGithubLine,
-  },
+  // {                                     ///////////Github
+  //   name: "Github",
+  //   link: "https://github.com/NakamuraKuhiniko",
+  //   Icon: RiGithubLine,
+  // },
 ];
 
 const Socials = () => {
